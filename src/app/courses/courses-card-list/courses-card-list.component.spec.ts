a940cbf6e1efa2c3978e92f87c692c8403ded297
PiotrPiwonski <49531459+PiotrPiwonski@users.noreply.github.com>
2023-11-21 12:41:28 +0100

    add tests in display the course list

--- src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -14,6 +14,7 @@ import {setupCourses} from '../common/setup-test-data';
 describe('CoursesCardListComponent', () => {
   let component: CoursesCardListComponent;
   let fixture: ComponentFixture<CoursesCardListComponent>;
+  let el: DebugElement;
 
   beforeEach(async (() => {
     TestBed.configureTestingModule({
@@ -23,20 +24,21 @@ describe('CoursesCardListComponent', () => {
       .then(() => {
         fixture = TestBed.createComponent(CoursesCardListComponent);
         component = fixture.componentInstance;
+        el = fixture.debugElement;
       });
   }) );
 
   it("should create the component", () => {
-
    expect(component).toBeTruthy();
-
   });
 
 
   it("should display the course list", () => {
-
-    pending();
-
+    component.courses = setupCourses();
+    const cards = el.queryAll(By.css('.course-card'));
+    expect(cards).toBeTruthy('Could not find cards');
+    expect(cards.length).toBe(12,
+      'Unexpected number of courses');
   });
 
 
